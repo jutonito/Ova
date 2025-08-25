769f05b5bdeb7627c71bc449b36aaf5646fd122c
Jutonito <juandavidariasarenas15@gmail.com>
2025-08-25 17:00:44 -0500

    Cambios palabras

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -15,8 +15,8 @@ export class MenuComponent {
   
   public texts : string[] = [
     "Hola bienvenido a la web de 'Buscando el Rastro'.",
-    "Me presento, soy Chiro, soy un investigador novato, y necesitaré de tu ayuda para mejorar mis habilidades",
-    "En este caso estoy investigando a la revistra 'Rastros Rostros', esta parece ser un gran recurso para aprender gran cantidad de temas interesantes",
+    "Me presento, soy Güiro, soy un investigador novato, y necesitaré de tu ayuda para mejorar mis habilidades",
+    "En este caso estoy investigando a la revista 'Rastros Rostros', esta parece ser un gran recurso para aprender gran cantidad de temas interesantes",
     "Sin embargo, no soy capaz de entenderla en su totalidad.",
     "Así que estaré dando un recorrido a indexes de la revista, acompañame a esta aventura y así aprenderas conmigo sobre la revista 'Rastros Rostros'",
   ]
